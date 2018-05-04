6e563068ac6fc6cf63aca4bd79724c8aafa29621
fred <fpinaud17@gmail.com>
2018-05-04 07:27:23 +0200

    db config api OK

--- apiKeys/db_config_api.php
+++ apiKeys/db_config_api.php
@@ -7,17 +7,14 @@
  */
 
 
-define('DB_USER', "");
-define('DB_PASSWORD', "");
-define('DB_DATABASE', "");
-define('DB_SERVER', '');
+define('DB_USER', "fred");
+define('DB_PASSWORD', "password");
+define('DB_DATABASE', "apiKey");
+define('DB_SERVER', 'raspberrysqlserver.ddns.net');
 
 $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
 $db = new PDO('mysql:host=' . DB_SERVER . ';dbname=' . DB_DATABASE, DB_USER, DB_PASSWORD, $pdo_options);
 $db->exec("Set character set utf8");
-if($db){
-    echo "connexion bdd apiKey ok !";
-};
 
 function VerifApi()
 {
